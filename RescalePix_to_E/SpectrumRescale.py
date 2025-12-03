2e7e275d4611e9eff75d0bc8bb99f6830f940473
aline-vernier <aline-vernier@users.noreply.github.com>
2025-12-03 13:04:02 +0100

    Added functions for both spectrometer versions
    To be added to GUI :)

---
+++ RescalePix_to_E/SpectrumRescale.py
@@ -0,0 +1,158 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from PIL import Image
+from pathlib import Path
+import time as t
+import pyqtgraph as pg
+
+
+class SpectrumRescale:
+
+    def __init__(self,
+                 imPath='.\\calib\\'
+                        'SET6\\SET6\\magnet0.4T_Soectrum_isat4.9cm_26bar_gdd25850_HeAr_0002.TIFF',
+                 calPath='.\\calib\\dsdE_Small_LHC.txt',
+                 pixel_per_mm=1,
+                 plotCal=False,
+                 deflection='L',
+                 **kwds):
+
+        self.imPath = imPath
+        self.calPath = calPath
+        self.pixel_per_mm = pixel_per_mm
+
+        self.E_file = None
+        self.E = None
+        self.dsdE_file = None
+        self.dsdE = None
+        self.s_file = None
+        self.s = None
+        self.im = None
+        self.zero_mm = 0
+        self.zero_px = 0
+        self.s_ref = None
+        self.deflection = deflection
+
+        self.y_scale = None
+
+        self.load_calib()
+        self.load_image()
+
+        if plotCal:
+            plt.plot(self.s, self.E)
+            plt.show()
+    '''Load calibration file. Currently the format is 
+    cal[0]:Energy in Mev
+    cal[1]:ds/dE in mm/MeV, currently unused
+    cal[2]:s in mm'''
+    def load_calib(self, calPath=None):
+        if calPath is not None:
+            self.calPath = calPath
+        cal = np.loadtxt(self.calPath).T
+        '''Data is flipped to allow usage of numpy interpolation'''
+        self.E_file = np.flip(cal[0])
+        self.dsdE_file = np.flip(cal[1])
+        self.s_file = np.flip(cal[2])
+
+    def load_image(self):
+        self.im = np.array(Image.open(self.imPath))
+
+    '''Placeholder for filtering out meaningless data'''
+    def remove_meaningless_points(self, x_array, y_array):
+        pass
+
+    '''TWO DIFFERENT SPECTROMETER GEOMETRIES :
+        - With zero (ref without magnet deflection)
+        - Without zero (ref only from geometry)'''
+
+    '''Rescaled1D_zero works with a zero in pixels or in mm.
+    Low energies are on the LEFT, flipping options to be added? 
+    Zero definition from cursors or automated to be added to interface.'''
+    def rescale1D_zero(self, zero_px=None, zero_mm=None):
+        if zero_px is not None:
+            self.zero_px = zero_px
+            self.zero_mm = zero_px/self.pixel_per_mm
+        if zero_mm is not None:
+            self.zero_mm = zero_mm
+            self.zero_px = zero_mm*self.pixel_per_mm
+
+        try:
+            offset = self.im.shape[1]-self.zero_px
+            self.s = (np.array(range(0, self.im.shape[1]))-offset)/self.pixel_per_mm
+            self.E = np.interp(self.s, self.s_file, self.E_file, right=float('nan'), left=float('nan'))
+
+        except Exception as e:
+            print(f'Exception: {e}')
+
+    def rescale2D_zero(self, zero_px=None, zero_mm=None):
+        self.rescale1D_zero(zero_px, zero_mm)
+        self.y_scale = np.array(range(0, self.im.shape[0]))/self.pixel_per_mm
+
+    '''Rescaled1D_ref works without a zero but with a reference point 
+    tuple (position in mm, energy in MeV).
+    
+        - refPoint defined from real lanex coordinate and energy E_ref
+        - s_ref calculated from interpolation of E_ref in calibration file
+        - Zero assumed on the right of the image (right edge of the lanex).
+        - Low energies are on the LEFT, flipping options to be added soon. 
+        
+    Reference point definition to be added to interface.'''
+
+    def rescale1D_ref(self, refPoint=(40,10)):# Default value, 40mm <=> 10MeV
+
+        self.s_ref = np.interp(refPoint[1], np.flip(self.E_file), np.flip(self.s_file)
+                               , right=float('nan'), left=float('nan')) # Flip not too clean, to be sanitized ;)
+        self.s = np.array(range(0, self.im.shape[1]))/self.pixel_per_mm-(self.s_ref - refPoint[0])
+        self.E = np.interp(self.s, self.s_file, self.E_file, right=float('nan'), left=float('nan'))
+
+    def rescale2D_ref(self, refPoint=(40,10)):
+        self.rescale1D_ref(refPoint)
+        self.y_scale = np.array(range(0, self.im.shape[0])) / self.pixel_per_mm
+
+
+if __name__ == "__main__":
+
+    '''Experimental data: spatial calibration 49µm/mm, zero position {x=1953px, y=635px}'''
+    r = SpectrumRescale(plotCal=False, pixel_per_mm=20)
+    '''Both functions work, to be thoroughly tested ;)'''
+    r.rescale2D_zero(zero_px=1953)
+    #r.rescale2D_ref((40,10))
+
+    '''Analysis'''
+    data = r.im # shape: (n_y, n_x)
+    x = r.E
+    y = r.y_scale
+
+    '''To be integrated to class'''
+    # Find valid x indices (non-NaN)
+    valid_mask = ~np.isnan(x)
+    x_valid = x[valid_mask]
+    data_valid = data[:, valid_mask]  # Keep all rows, filter columns
+
+    '''Plot, PyQtGraph'''
+    app = pg.mkQApp()
+    win = pg.GraphicsLayoutWidget(show=True, title="2D Data with Valid X")
+    plot = win.addPlot(title="Filtered Data")
+
+    # Create ImageItem
+    img = pg.ImageItem()
+    plot.addItem(img)
+
+    # Set the image data
+    img.setImage(data_valid.T)  # Note: transpose for correct orientation
+
+    # Scale and position the image to match your axes
+    # Calculate the transform to map pixel coordinates to data coordinates
+    scale_x = (x_valid[-1] - x_valid[0]) / (len(x_valid) - 1)
+    scale_y = (y[-1] - y[0]) / (len(y) - 1)
+    img.setTransform(pg.QtGui.QTransform().translate(x_valid[0], y[0]).scale(scale_x, scale_y))
+
+    # Add colorbar
+    colorbar = pg.ColorBarItem(values=(data_valid.min(), data_valid.max()), colorMap='viridis')
+    colorbar.setImageItem(img)
+    win.addItem(colorbar)
+
+    app.exec()
+
+
+
